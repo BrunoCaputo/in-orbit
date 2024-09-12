9812962f9541855029d5a58fe291f4b8fd665c5e
BrunoCaputo <brunopc.dev@gmail.com>
2024-09-11 23:09:04 -0300

    feat: Added initial page
    Empty goals page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,23 @@
+import { Plus } from "lucide-react";
+
+import logo from "./assets/logo.svg";
+import letsStart from "./assets/lets-start.svg";
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return <></>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit Logo" />
+      <img src={letsStart} alt="in.orbit Logo" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal{" "}
+        <span className="underline">cadastrar um</span> agora mesmo?
+      </p>
+
+      <Button>
+        <Plus className="size-4" />
+        Cadastrar meta
+      </Button>
+    </div>
+  );
 }
