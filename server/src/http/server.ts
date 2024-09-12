e3ce5f73a0deaebf43eb04f4a051f9d3994465b4
BrunoCaputo <brunopc.dev@gmail.com>
2024-09-11 21:32:12 -0300

    feat: Prepare to get week summary

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -7,6 +7,7 @@ import {
 import { createGoalRoute } from "./routes/create-goal.route";
 import { getPendingGoalsRoute } from "./routes/get-pending-goals.route";
 import { createCompletionRoute } from "./routes/create-completion.route";
+import { getWeekSummaryRoute } from "./routes/get-week-summary.route";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -16,6 +17,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(getPendingGoalsRoute);
 app.register(createGoalRoute);
 app.register(createCompletionRoute);
+app.register(getWeekSummaryRoute);
 
 app
   .listen({
