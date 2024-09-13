96302119527fdc2b6c8a079346027edaf37f484c
BrunoCaputo <brunopc.dev@gmail.com>
2024-09-12 22:47:16 -0300

    feat: Goals summary

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import { Dialog } from "./components/ui/dialog";
 import { CreateGoalDialog } from "./components/CreateGoalDialog";
-import { EmptyGoals } from "./components/EmptyGoals";
+import { Summary } from "./components/Summary";
 
 export function App() {
   return (
     <Dialog>
-      <EmptyGoals />
+      {/* <EmptyGoals /> */}
+      <Summary />
 
       <CreateGoalDialog />
     </Dialog>
